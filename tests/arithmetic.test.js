da168596b3a6b1fac27201edef8c8419b0093f02
Alex Rankin <alex.c.rankin@gmail.com>
2018-09-24 15:44:08 -0700

    split up NaN assertions into multiple tests

--- tests/arithmetic.test.js
+++ tests/arithmetic.test.js
@@ -2,20 +2,27 @@ const assert = require('assert');
 const { add, subtract } = require('../lib/arithmetic');
 
 describe('arithmetic', () =>{
+    
     it('should add two numbers', () => {
         const sum = add(2, 5);
         assert.equal(sum, 7);
     });
+
     it('should subtract two numbers', () => {
         const difference = subtract(2, 5);
         assert.equal(difference, -3);
     });
-    it('should throw an error if given non-number input', () => {
+
+    it('add should throw an error if given non-number input', () => {
         assert.throws(() => {
             add(5, 'ham');
         }, Error);
-        assert.throws(() => {
-            subtract(2, 'cheese');
-        }, Error);
     });
+
+    it('subtract should throw an error if given non-number input', () => {
+        assert.throws(() => {
+            subtract(2, 'bologna');
+        }, Error);
+    })
+
 });
