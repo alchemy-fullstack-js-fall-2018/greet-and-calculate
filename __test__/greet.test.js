c3f9397fb3b6f9c907c3e852fd7390279f074690
KLPainter <KLPainter@gmail.com>
2018-09-24 14:28:22 -0700

    passing one test

---
+++ __test__/greet.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const greet = require('../lib/greet.js');
+
+describe('greetings', () => {
+
+    it('greets the supplied name', () => {
+        const greeting = greet('world');
+        assert.equal(greeting, 'hello world');
+    });
+
+    it('throws an error when name is not a string', () => {
+        if (typeOf )
+    });
+
+});
