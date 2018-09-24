a207d4f6ca35abae7d5badf48876d2987e630bb8
Jack <ajackofsometrades@gmail.com>
2018-09-24 14:57:54 -0700

    red/green for sub working with numbers

--- test/arithmetic.test.js
+++ test/arithmetic.test.js
@@ -1,8 +1,7 @@
 const assert = require('assert');
-const add = require('../lib/arithmetic');
+const { add, sub } = require('../lib/arithmetic');
 
 describe('add', () => {
-
 
     it('receives two numbers', () => {
         assert.throws(() => {
@@ -13,6 +12,15 @@ describe('add', () => {
     it('returns sum of two numbers', () => {
         const sum = add(2, 3);
         assert.equal(sum, 5);
+    });
+
+});
+
+describe('sub', () => {
+
+    it('returns the difference of two numbers', () => {
+        const diff = sub(3, 2);
+        assert.equal(diff, 1);
     });
 
 });
