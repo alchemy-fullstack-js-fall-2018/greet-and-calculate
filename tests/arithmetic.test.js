c4c17406806fa3753a9a526454a295696e75fc8b
Alex Rankin <alex.c.rankin@gmail.com>
2018-09-24 15:17:21 -0700

    subtraction is working

--- tests/arithmetic.test.js
+++ tests/arithmetic.test.js
@@ -1,9 +1,13 @@
 const assert = require('assert');
-const add = require('../lib/arithmetic');
+const { add, subtract } = require('../lib/arithmetic');
 
 describe('arithmetic', () =>{
     it('should add two numbers', () => {
         const sum = add(2, 5);
         assert.equal(sum, 7);
+    });
+    it('should subtract two numbers', () => {
+        const difference = subtract(2, 5);
+        assert.equal(difference, -3);
     });
 });
