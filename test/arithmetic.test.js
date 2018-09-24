80a00bc92c44d365db93fd4d6634b0a158734a08
Sarah Flynn <sarah.flynn@icloud.com>
2018-09-24 16:39:15 -0700

    subtract function and all subtraction tests working

--- test/arithmetic.test.js
+++ test/arithmetic.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { add } = require('../lib/arithmetic');
+const { add, subtract } = require('../lib/arithmetic');
 
 describe('addition', () => {
     
@@ -28,5 +28,33 @@ describe('addition', () => {
         const sum = add('3.1789', '1.4567');
         assert.equal(sum, 4.6356);
     });
+    
+    it('subtracts two numbers supplied as arguments', () => {
+        const difference = subtract(40, 60);
+        assert.equal(difference, -20);
+    });
+    
+    it('throws error when one or both of the arguments are NaN', () => {
+        assert.throws(() => {
+            subtract('a', 5);
+        }, Error);
+    });
+    
+    it('subtracts numbers in the form of strings when they are provided as arguments', () => {
+        const difference = subtract('36', '12');
+        assert.equal(difference, 24);
+    });
+
+    it('subtracts floating point numbers', () => {
+        const difference = subtract(3.1789, 1.4567);
+        assert.equal(difference, 1.7222);
+    });
+    
+    it('subtracts floating point numbers in the form of strings when they are provided as arguments', () => {
+        const difference = subtract('3.1789', '1.4567');
+        assert.equal(difference, 1.7222);
+    });
+
+
 
 });
